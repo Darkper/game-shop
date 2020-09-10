55b08b9bae75d9c63a5c0497443eb707bcdc1624
Johny Soto <jaxsotoo@outlook.com>
2020-09-10 00:27:31 -0500

    Commit inicial

---
+++ src/main/java/com/jaxso/gameshop/GameShopApplication.java
@@ -0,0 +1,13 @@
+package com.jaxso.gameshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameShopApplication.class, args);
+    }
+
+}
